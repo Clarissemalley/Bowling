a1f0bf8ed5de76d37447abf8290f83fe16d1f1e1
Mela-nie <46560863+Mela-nie@users.noreply.github.com>
2019-03-14 11:50:33 +0100

    Update Score.py

--- Score.py
+++ Score.py
@@ -7,10 +7,14 @@ fenetre = Tk()
 
 Label(fenetre, text='Tour', borderwidth=1).grid(row=1, column=1)
 Label(fenetre, text='J1', borderwidth=1).grid(row=2, column=1)
+p = IntVar(0)
 s = IntVar(0)
+p.set(2)
 s.set(8) #s=8
 for colonne in range(2, 13):
     Label(fenetre, text=str(colonne-1), borderwidth=1).grid(row=1, column=colonne)
-    Label(fenetre, textvar=s, borderwidth=1).grid(row=2, column=colonne)
+    Label(fenetre, textvar=s, borderwidth=1).grid(row=2, column=2)
+    Label(fenetre, textvar=p, borderwidth=1).grid(row=2, column=3)
 Label(fenetre, text='Total', borderwidth=1).grid(row=1, column=12)
+
 fenetre.mainloop()
