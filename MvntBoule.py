24e4707fbad0ec23fb16e4aaa804ca1f6bb2970e
Jair Bolsonaro <46561034+SvarD23@users.noreply.github.com>
2019-03-28 11:54:17 +0100

    Add files via upload

--- MvntBoule.py
+++ MvntBoule.py
@@ -1,48 +1,54 @@
-from tkinter import *
- 
+import tkinter as tk
+
+Pos_Xba=200
+Pos_Yba=330
+
+Pos_Xqu=200
+Pos_Yqu=20
+
 def deplacement():
     global dx, dy
     x1, y1, x2, y2 = canvas.coords(balle1)
-    if y1<- canvas.coords(quille)[3]:
+    if y1<canvas.coords(quille)[3]:
         dy=-1*dy
-    #On deplace la balle :
     canvas.move(balle1,dx,dy)
-    #On repete cette fonction
-    tk.after(20,deplacement)
-    if y1== canvas.coords(quille)[3]:
+    fen.after(20,deplacement)
+    if y1 == canvas.coords(quille)[3]:
         canvas.coords(balle1,x1,y1,x2,y2)
-#Une fonction pour modifier le deplacement vers la droite :
+
 def haut(event):
     global dx, dy
     dx=0
     dy=-5
- 
-#Coordonnées de la balle au départ:
-Pos_Xba=200
-Pos_Yba=330
-#Déplacement de la balle au départ:
-dx=0
-dy=0
 
-Pos_Xqu=200
-Pos_Yqu=20
-#On cree une fenêtre et un canevas:
-tk = Tk()
-canvas = Canvas(tk,width = 500, height = 400 , bd=0, bg="white")
+def Droite():
+    x1, y1, x2, y2 = canvas.coords(balle1)
+    canvas.coords(balle1,x1+20,y1,x2+20,y2)
+
+def Gauche():
+    x1, y1, x2, y2 = canvas.coords(balle1)
+    canvas.coords(balle1,x1-20,y1,x2-20,y2)
+
+fen = tk.Tk()
+canvas = tk.Canvas(fen,width = 500, height = 400 , bd=0, bg="white")
 canvas.pack(padx=10,pady=10)
- 
-#Création  d'un bouton "Quitter":
-Bouton_Quitter=Button(tk, text ='Quitter', command = tk.destroy)
-#On ajoute l'affichage du bouton dans la fenêtre tk:
-Bouton_Quitter.pack()
- 
-#On cree une balle:
+
 balle1 = canvas.create_oval(Pos_Xba,Pos_Yba,Pos_Xba+50,Pos_Yba+50,fill='red')
 quille= canvas.create_oval(Pos_Xqu,Pos_Yqu,Pos_Xqu+20,Pos_Yqu+20,fill='black')
  
+dx=0
+dy=0
+
+
+Bouton_Quitter=tk.Button(fen, text ='Quitter', command = fen.destroy)
+Bouton_Quitter.pack()
+
 canvas.bind_all('<Up>', haut)
  
-deplacement()
+tk.Button(fen,text='Droite',command=Droite).pack()
+tk.Button(fen,text='Gauche',command=Gauche).pack()
+
  
-#On lance la boucle principale:
-tk.mainloop()
+deplacement()
+
+fen.mainloop()
